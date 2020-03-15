7b195fb3f911914a181dd44893873ee4cd71c97c
Vinicius <vinicius.batista.barbosa34@gmail.com>
2020-03-15 18:45:02 +0000

    Adicionando Cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://omnistack10:omnistack10@cluster0-9tucm.mongodb.n
     useUnifiedTopology: true,
 })
 
+app.use(cors());
 app.use(express.json()); 
 app.use(routes);
 

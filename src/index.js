31fdbe756b53e475cfd015aaed15ed494349edf4
Vinicius <vinicius.batista.barbosa34@gmail.com>
2020-03-17 13:36:46 +0000

    Adicionando .env

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import routes from './routes';
@@ -5,7 +6,7 @@ import cors from 'cors';
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack10:omnistack10@cluster0-9tucm.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
